4554f33a27ac79d87ce9abda6d6dbe94e9930d04
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-05-12 14:58:10 -0600

    test: add tests to check path_finder method

--- test/path_finder.rb
+++ test/path_finder.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 require_relative '../lib/path_finder'
+require_relative '../lib/board'
+require_relative '../lib/knight'
 
 # Test script helps debug the
 # path finder class 
@@ -8,14 +10,22 @@ require_relative '../lib/path_finder'
 # 
 module TestPathFinder
   class << self
-    attr_accessor :path_finder
+    attr_accessor :board, :path_finder, :start, :destination
   end
 
   def self.setup
-    @path_finder = PathFinder.new(start, destination)
+    @board = Board.new(8)
+    @start = [0, 0]
+    @destination = [3, 3]
+    p @path_finder = PathFinder.new
+  end
+
+  def self.path
+    path_finder.find_path(board, start, destination)
   end
 end
 
 if __FILE__ == $PROGRAM_NAME
-  puts TestPathFinder.setup
+  TestPathFinder.setup
+  p TestPathFinder.path
 end
