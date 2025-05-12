f8762cd46329f955b49ac088743afe74c8bb7e2c
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-05-12 11:53:42 -0600

    test: add new tests for board rules on valid moves

--- test/board.rb
+++ test/board.rb
@@ -14,8 +14,17 @@ module TestBoard
     @board = Board.new
     board.inspect
   end
+
+  def self.valid_moves
+    p board.valid_moves(3, 3)
+    p board.valid_moves(0, 0)
+    p board.valid_moves(2, 4)
+    p board.valid_moves(7, 7)  
+    p board.valid_moves(5, 4)
+  end
 end
 
 if __FILE__ == $PROGRAM_NAME
-  puts TestBoard.setup
+  TestBoard.setup
+  TestBoard.valid_moves
 end
