6e9bd4536c0640e746c4c088fd328f8637ebf663
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-05-12 14:57:40 -0600

    feat(WIP): add BFS path finder logic with queuing

--- lib/path_finder.rb
+++ lib/path_finder.rb
@@ -15,11 +15,43 @@
 # path = PathFinder.new
 #
 class PathFinder
-  attr_accessor :queue, :visited, :path_history
+  attr_accessor :queue, :visited, :path_history 
 
-  def initialize(knight)
-    @queue = knight.start_position
+  def initialize
+    @queue = []
     @visited = []
     @path_history = {}
+  end
+
+  def find_path(board, start, destination)
+    return nil if start == destination
+    
+    queue = [start]
+    path_history[start] = nil
+
+    until queue.empty? || start == destination
+      current = queue.shift
+      @visited << current
+      path_history[visited[-1]] = visited[-2]
+      queue.concat(board.valid_moves(current[0], current[-1])) 
+      # TODO: new method that filters out already visited valid moves
+      # also need logic where if the valid moves returned contains the 
+      # destination, let's go there first?
+
+      return retrace_path(start, destination) if current == destination
+    end
+    retrace_path(start, destination)
+  end
+
+  private
+
+  def retrace_path(start, destination)
+    current = destination
+    result = [destination]
+    until path_history[current].nil?
+      result.unshift(path_history[current])
+      current = result[0]
+    end
+    result
   end
 end
